6598de60fbc299c549b1c14471f2398427060da0
Koni <konichan619@gmail.com>
2023-07-19 16:03:40 +0800

    Add HTML webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "Weather App",
+            template: "./src/test.html",
         }),
         new MiniCssExtractPlugin({}),
     ],
